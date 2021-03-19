b3048667adf55a5079de2a92df760ed836afe4f5
Ednan Jhony <ednanjhony@yahoo.com.br>
2021-03-19 08:02:23 -0300

    configured contextApi

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
 
 import { Globalstyle } from "./styles/global";
 
@@ -20,7 +21,7 @@ export function App() {
 	}
 
   return (
-    <>
+    <TransactionsProvider>
 			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
 			<Dashboard />
@@ -31,7 +32,7 @@ export function App() {
 			/>
 
 			<Globalstyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
